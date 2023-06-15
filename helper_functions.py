29853ef740e646c5c0f44c89eb75914c4f10907e
Martin Løland <martin.loland@4subsea.com>
2023-06-15 12:25:16 +0200

    added stormgeo function

--- helper_functions.py
+++ helper_functions.py
@@ -1,6 +1,22 @@
+import os
+
 import pandas as pd
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_URL = os.environ["API_URL"]
 
 
 def generate_df():
     data = {"a": [1, 2, 3], "b": [4, 5, 6]}
     return pd.DataFrame(data)
+
+
+def get_api_data():
+    response = requests.get(API_URL)
+    print(response.json())
+
+
+get_api_data()
